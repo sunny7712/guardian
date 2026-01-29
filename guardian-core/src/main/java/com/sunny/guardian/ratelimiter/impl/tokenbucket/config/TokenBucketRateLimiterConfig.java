24c93151010e287d734fc29f73bc7c26707f15b0
sunny7712 <sunny77katta2002@gmail.com>
2026-01-29 22:03:33 +0530

    removed older Storage.java and it's implementation

--- guardian-core/src/main/java/com/sunny/guardian/ratelimiter/impl/tokenbucket/config/TokenBucketRateLimiterConfig.java
+++ guardian-core/src/main/java/com/sunny/guardian/ratelimiter/impl/tokenbucket/config/TokenBucketRateLimiterConfig.java
@@ -1,31 +1,14 @@
 package com.sunny.guardian.ratelimiter.impl.tokenbucket.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sunny.guardian.config.BaseRateLimiterConfig;
 import com.sunny.guardian.ratelimiter.impl.tokenbucket.dto.TokenBucketQuota;
-import com.sunny.guardian.ratelimiter.impl.tokenbucket.dto.TokenBucketState;
-import com.sunny.guardian.storage.Storage;
-import com.sunny.guardian.storage.impl.RedisTransactionStorage;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @ConfigurationProperties(prefix = "guardian.token-bucket")
 public class TokenBucketRateLimiterConfig extends BaseRateLimiterConfig<TokenBucketQuota> {
 
-    @Bean("tokenBucketRedisStorage")
-    public Storage<TokenBucketState> tokenBucketRedisStorage(
-            RedisTemplate<String, String> redisTemplate,
-            ObjectMapper objectMapper
-    ) {
-        return new RedisTransactionStorage<>(
-                redisTemplate,
-                objectMapper,
-                TokenBucketState.class
-        );
-    }
 
 
 }
--- guardian-core/src/main/java/com/sunny/guardian/storage/Storage.java
+++
@@ -1,8 +0,0 @@
-package com.sunny.guardian.storage;
-
-import java.util.function.BiFunction;
-
-public interface Storage<T> {
-    T compute(String key, BiFunction<String, T, T> remappingFunction);
-    T get(String key);
-}
